918219873c786e5fd407fbaa45afa62dd3a46a37
Thanduxolo Nodwengu <nodwengut@gmail.com>
2020-06-19 16:00:33 +0200

    added a default greeting

--- src/test/java/net/greet/GreetingsTest.java
+++ src/test/java/net/greet/GreetingsTest.java
@@ -2,7 +2,6 @@ package net.greet;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import java.sql.*;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -13,14 +12,9 @@ public class GreetingsTest {
    @Test
    public void connect() {
       PersonService personService = new PersonService();
-
-      try {
-         personService.connectToDatabase();
-         personService.delete();
-         
-      } catch (Exception e) {
-         e.printStackTrace();
-      }
+   
+      personService.connectToDatabase();
+      personService.delete();
    }
    
    @Test
@@ -31,165 +25,75 @@ public class GreetingsTest {
       p1 = new Person();
       p2 = new Person();
       p3 = new Person();
-
-      try {
-         assertEquals(0, personService.getAll().size());
-
-         p1.setUsername("thando");
-         p2.setUsername("james");
-         p3.setUsername("jola");
-
-         personService.add(p1);
-         personService.add(p2);
-         personService.add(p3);
-
-         assertEquals(3, personService.getAll().size());
-
-      } catch (SQLException e) {
-         System.out.println(e.getMessage());
-         System.out.println("shouldBeAbleToAddNewUser");
-      }
+   
+      p1.setUsername("thando");
+      p2.setUsername("james");
+      p3.setUsername("jola");
+   
+      personService.add(p1);
+      personService.add(p2);
+      personService.add(p3);
+   
+      assertEquals(3, personService.getAll().size());
    }
    
    @Test
    public void shouldBeAbleToIncreaseGreetCounter() {
       PersonService personService = new PersonService();
       Person person = null;
-//      try {
-//         personService.allGreeted();
-//      } catch (SQLException e) {
-//
-//      }
-
-      try {
-         person = personService.getByName("thando");
-        
-         assertEquals(1, person.getGreetCount());
-
-         person.increaseCounter();
-         personService.updateGreetCount(person, person.getGreetCount());
-
-         assertEquals(2, person.getGreetCount());
-
-      } catch (SQLException e) {
-         System.out.println(e.getMessage());
-         System.out.println("shouldBeAbleToIncreaseGreetCounter");
-      }
+   
+      person = personService.getByName("thando");
+   
+      assertEquals(1, person.getGreetCount());
+   
+      person.increaseCounter();
+      personService.updateGreetCount(person, person.getGreetCount());
+   
+      assertEquals(2, person.getGreetCount());
    }
    
    @Test
    public void shouldBeAbleToDecreaseGreetCounter() {
       PersonService personService = new PersonService();
       Person person = null;
-
-      try {
-         person = personService.getByName("thando");
-
-         person.increaseCounter();
-         personService.updateGreetCount(person, person.getGreetCount());
-
-         assertEquals(3, person.getGreetCount());
-
-         person.decreaseCounter();
-         personService.updateGreetCount(person, person.getGreetCount());
-
-         assertEquals(2, person.getGreetCount());
-
-      } catch (SQLException e) {
-         System.out.println(e.getMessage());
-         System.out.println("shouldBeAbleToDecreaseGreetCounter");
-         
-      }
+   
+      person = personService.getByName("thando");
+   
+      person.increaseCounter();
+      personService.updateGreetCount(person, person.getGreetCount());
+   
+      assertEquals(3, person.getGreetCount());
+   
+      person.decreaseCounter();
+      personService.updateGreetCount(person, person.getGreetCount());
+   
+      assertEquals(2, person.getGreetCount());
    }
 
    @Test
    public void shouldBeAbleToRemoveUser() {
       PersonService personService = new PersonService();
       Person person = null;
-
-      try {
-         assertEquals(3, personService.getAll().size());
-         
-         person = personService.getByName("jola");
-         personService.remove(person);
    
-         assertEquals(2, personService.getAll().size());
-         
-      } catch (SQLException e) {
-         System.out.println(e.getMessage());
-         System.out.println("shouldBeAbleToRemoveUser");
-      }
+      assertEquals(3, personService.getAll().size());
+   
+      person = personService.getByName("jola");
+      personService.remove(person);
+   
+      assertEquals(2, personService.getAll().size());
    }
    
    @Test
    public void shouldBeAbleToReturnUserByName() {
       PersonService personService = new PersonService();
       Person person = null;
-      
-      try {
-         person = personService.getByName("james");
-         // personService.allGreeted();
-         assertEquals("james", person.getUsername());
-         
-         assertEquals(3, personService.getAll().size());
-         
-      } catch (SQLException e) {
-         System.out.println(e.getMessage());
-         System.out.println("shouldBeAbleToReturnUserByName");
-      }
+   
+      person = personService.getByName("james");
+      // personService.allGreeted();
+      assertEquals("james", person.getUsername());
+   
+      assertEquals(3, personService.getAll().size());
    }
-
-
-//   public void shouldBeAbleToGreetByLanguage() {
-//      personService = new PersonService();
-//      greet = new Greet();
-//      Person person = null;
-//
-//      assertEquals("Mholo thando", greet.greetUser("thando", "Xhosa"));
-//
-//      //assertEquals("Mholo thando", greet.greetUser("thando", "afrikaans"));
-//
-//      //assertEquals("Mholo thando", greet.greetUser("thando", "english"));
-//
-//   }
-
-
-
-
-//   public void shouldBeAbleToGreetInXhos() {
-//      personService = new PersonService();
-//      greet = new Greet();
-//
-//      try {
-//         personService.allGreeted();
-//      } catch (SQLException e) {
-//         e.printStackTrace();
-//      }
-//
-//      //greet.greet("thando", "xhosa");
-//
-//
-//
-//      //
-//   }
    
    
-   
-   
-   
-   
-//
-//   @Test
-//   public void shouldBeAbleToRemoveAll() {
-//      PersonService personService = new PersonService();
-//      personService.delete();
-//   }
-
-
-
-
-
-
-
-
 }
